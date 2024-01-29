ffb4fb684954e1758ce995b637303571eb497ded
Nikhil <nikhilganeshpatil@gmail.com>
2024-01-29 22:07:52 +0530

    I have implemented the prisma part of activation email.

--- src/lib/actions/authActions.ts
+++ src/lib/actions/authActions.ts
@@ -4,6 +4,8 @@ import { User } from "@prisma/client"
 import prisma from "../prisma"
 import * as bcrypt from "bcrypt"
 import { compileActivationTemplate, sendMail } from "../mail"
+import { signJwt } from "../jwt"
+import { verify } from "jsonwebtoken"
 
 type UserType = {
   password: string
@@ -22,8 +24,8 @@ export async function registerUser(
       password: await bcrypt.hash(user.password, 10),
     },
   })
-
-  const activationUrl = `${process.env.NEXTAUTH_URL}/auth/activation/${result.id}`
+  const jwtUserId = signJwt({ id: result.id })
+  const activationUrl = `${process.env.NEXTAUTH_URL}/auth/activation/${jwtUserId}`
 
   const body = compileActivationTemplate(user.firstname, activationUrl)
 
@@ -33,4 +35,35 @@ export async function registerUser(
     body: body,
   })
   return result
+}
+
+type ActivateUserFunc = (
+  jwtUserId: string
+) => Promise<"userNotExist" | "alreadyActivated" | "success">
+
+export const activateUser: ActivateUserFunc = async (jwtUserId) => {
+  const secretKey = process.env.USER_ID_SECRET
+  const payload = verify(jwtUserId, secretKey!)
+  const userId = payload?.id
+  const user = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+  })
+
+  if (!user) {
+    return "userNotExist"
+  }
+  if (user.emailVerified) {
+    return "alreadyActivated"
+  }
+  const result = await prisma.user.update({
+    where: {
+      id: userId,
+    },
+    data: {
+      emailVerified: new Date(),
+    },
+  })
+  return "success"
 }
